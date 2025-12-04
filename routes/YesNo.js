b9348b7fad52b71cf12f2acebd6137f274791434
nishimoto71 <kn5252@gmail.com>
2025-12-04 15:50:34 +0900

    change 2

--- routes/YesNo.js
+++ routes/YesNo.js
@@ -1,23 +1,13 @@
-// routes/YesNo.js
-const express = require('express');
-const router = express.Router();
-
+var express = require('express');
+var router = express.Router();
 const request = require('request');
-
-router.get('/', function(req, res, next) {
-  request('https://yesno.wtf/api', function (error, response, body) {
-    if (error) return next(error);
-    if (response.statusCode !== 200) return next(new Error('Bad status code: ' + response.statusCode));
-    let data;
-    try {
-      data = JSON.parse(body);
-    } catch (e) {
-      return next(e);
-    }
-    // レンダリングまたはJSON応答
-    res.render('YesNo', { answer: data.answer, image: data.image });
-    // または res.json({ answer: data.answer, image: data.image });
-  });
+router.get('/', async (req, res) => {
+request('https://yesno.wtf/api', function (error, response,
+body) {
+if (!error && response.statusCode == 200) {
+const data = JSON.parse(body);
+res.json(data);
+}
 });
-
+})
 module.exports = router;
