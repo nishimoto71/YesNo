80dd1e5c9d9008485a942e02f24fbb6ba5da9739
nishimoto71 <kn5252@gmail.com>
2025-12-04 15:34:58 +0900

    change YesNo

--- routes/YesNo.js
+++ routes/YesNo.js
@@ -1,9 +1,23 @@
+// routes/YesNo.js
+const express = require('express');
+const router = express.Router();
+
 const request = require('request');
-request('https://yesno.wtf/api', function (error, response, body) {
-if (!error && response.statusCode == 200) {
-const data = JSON.parse(body);
-console.log(body);
-const catImageUrl = data[0].url;
-console.log(catImageUrl);
-}
+
+router.get('/', function(req, res, next) {
+  request('https://yesno.wtf/api', function (error, response, body) {
+    if (error) return next(error);
+    if (response.statusCode !== 200) return next(new Error('Bad status code: ' + response.statusCode));
+    let data;
+    try {
+      data = JSON.parse(body);
+    } catch (e) {
+      return next(e);
+    }
+    // レンダリングまたはJSON応答
+    res.render('YesNo', { answer: data.answer, image: data.image });
+    // または res.json({ answer: data.answer, image: data.image });
+  });
 });
+
+module.exports = router;
